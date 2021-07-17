32e7662fd69c15a70e5b5c15a53a0363e32a1324
wuyanfeiying <wuyanfeiying@163.com>
2021-07-17 20:38:27 +0800

    自动化构建

--- leetCode/js/7.整数反转.js
+++ leetCode/js/7.整数反转.js
@@ -31,6 +31,8 @@ var reverse = function(x) {
   // 如果反转后整数超过 32 位的有符号整数的范围 [−2**31,  2**31 − 1] ，就返回 0。
   return 0
 };
-const result = reverse(1534236469)
+console.log(reverse(123)); // 321
+console.log(reverse(-123)); // -123
+console.log(reverse(120)); // 21
+console.log(reverse(0)); // 0
 // @lc code=end
-
