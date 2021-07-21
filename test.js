176fa37fa209065d73f15e80c8dc1d45da64dac1
chuhongguang <chuhongguang@lxrental.com>
2021-07-21 17:39:39 +0800

    函数柯里化

--- test.js
+++ test.js
@@ -4,14 +4,13 @@
  * @Description: 测试
  */
 
-// 比较两个数大小的纯函数
 function compareSize(b) {
   return function (a) {
     return a > b
   }
 }
 
-const compareNumTwo = compareSize(2)
-console.log(compareNumTwo(1)); // false
-console.log(compareNumTwo(2)); // false
-console.log(compareNumTwo(3)); // true
+const hasThanTarget = compareSize(2)
+console.log(hasThanTarget(1)); // false
+console.log(hasThanTarget(2)); // false
+console.log(hasThanTarget(3)); // true
