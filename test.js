655b63113d2b1b57b7c341e26d5024d747475b1f
chuhongguang <chuhongguang@lxrental.com>
2021-11-11 20:10:06 +0800

    整理面试题

--- test.js
+++ test.js
@@ -2,19 +2,4 @@
  * @Date: 2021-07-05 08:59:36
  * @LastEditors: chuhongguang
  */
-function add(...args) {
-  let final = [...args];
-  setTimeout(() => {
-    // final.reduce((sum, cur) => sum + cur)
-    console.log(final.reduce((sum, cur) => sum + cur));
-    // const a =  final.reduce((sum, cur) => sum + cur)
-    // console.log(a);
-  }, 0);
-  const inner = function (...args) {
-    final = [...final, ...args];
-    return inner;
-  };
-  return inner;
-}
-// add(1)(2)(3)
-console.log(add(1)(2)(3),'55'); //6
+
